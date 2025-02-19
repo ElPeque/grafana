1dfb90c49cd1351f4707a9cafde01d0a7d08b429
Igor Suleymanov <igor.suleymanov@grafana.com>
2025-02-19 15:42:02 +0200

    WIP
    Signed-off-by: Igor Suleymanov <igor.suleymanov@grafana.com>

--- pkg/apis/dashboard/v2alpha1/conversion_test.go
+++ pkg/apis/dashboard/v2alpha1/conversion_test.go
@@ -1,69 +1,10 @@
 package v2alpha1
 
 import (
-	"encoding/json"
 	"testing"
-
-	common "github.com/grafana/grafana/pkg/apimachinery/apis/common/v0alpha1"
-	"github.com/stretchr/testify/require"
 )
 
 func TestConvertDashboardVersions(t *testing.T) {
-	dashboardV0Spec := []byte(`{
-    "annotations": {
-      "list": [
-        {
-          "builtIn": 1,
-          "datasource": {
-            "type": "grafana",
-            "uid": "-- Grafana --"
-          },
-          "enable": true,
-          "hide": true,
-          "iconColor": "rgba(0, 211, 255, 1)",
-          "name": "Annotations \u0026 Alerts",
-          "type": "dashboard"
-        }
-      ]
-    },
-    "refresh": true,
-    "description": "",
-    "editable": true,
-    "fiscalYearStartMonth": 0,
-    "graphTooltip": 0,
-    "id": 11711,
-    "links": [],
-    "panels": [],
-    "preload": false,
-    "schemaVersion": 39,
-    "tags": [],
-    "templating": {
-      "list": []
-    },
-    "timepicker": {},
-    "timezone": "utc",
-    "title": "New dashboard",
-    "uid": "be3ymutzclgqod",
-    "version": 1,
-    "weekStart": ""
-}`)
-	object := common.Unstructured{}
-	err := json.Unmarshal(dashboardV0Spec, &object.Object)
-	require.NoError(t, err)
-
-	// TODO (@radiohead): un-comment this once we have the conversion code.
-
-	// convert v0 to v2, where we should extract the title & all other elements should be copied
-	result := DashboardSpec{}
-	err = Convert_v0alpha1_Unstructured_To_v2alpha1_DashboardSpec(&object, &result, nil)
-	require.NoError(t, err)
-	require.Equal(t, result.Title, "New dashboard")
-	// require.Equal(t, result.Unstructured, object)
-	// require.Equal(t, result.Unstructured.Object["refresh"], "", "schemaVersion migration not applied. refresh should be an empty string")
-
-	// now convert back & ensure it is the same
-	object2 := common.Unstructured{}
-	err = Convert_v2alpha1_DashboardSpec_To_v0alpha1_Unstructured(&result, &object2, nil)
-	require.NoError(t, err)
-	require.Equal(t, object, object2)
+	// TODO (@radiohead):
+	// Conversion to / from v2 is not supported.
 }
